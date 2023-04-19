c1ef2548db04ff691c6ed23fcb92a99fb567324b
GuntherBeginner <tanavit.c64@rsu.ac.th>
2023-04-20 00:58:31 +0700

    add home path

--- server.js
+++ server.js
@@ -10,10 +10,15 @@ require('dotenv').config()
 });*/
 
 const connection = mysql.createConnection(process.env.DATABASE_URL)
+console.log('Connected to PlanetScale!')
 //
 var app = express()
 app.use(cors())
 app.use(express.json())
+
+app.get('/', function (req, res, next) {
+    res.json("Hello World")
+})
 
 app.get('/users', function (req, res, next) {
     connection.query(
